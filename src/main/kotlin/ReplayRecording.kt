f5f03997a6b8f39415d8cd4038c1bb173c0b0bff
Tauktes <torbenstarn@gmail.com>
2025-07-17 23:02:06 +0200

    import handlers

--- src/main/kotlin/ReplayRecording.kt
+++ src/main/kotlin/ReplayRecording.kt
@@ -8,8 +8,16 @@ import com.comphenix.protocol.events.PacketEvent
 import org.bukkit.Bukkit
 import org.bukkit.plugin.ServicePriority
 import org.bukkit.plugin.java.JavaPlugin
-
+import org.realparkourhelper.handlers.handleArmAnimation
+import org.realparkourhelper.handlers.handleBlockDig
+import org.realparkourhelper.handlers.handleBlockPlace
 import org.realparkourhelper.handlers.handleChat
+import org.realparkourhelper.handlers.handleEntityAction
+import org.realparkourhelper.handlers.handleFlying
+import org.realparkourhelper.handlers.handleHeldItemSlot
+import org.realparkourhelper.handlers.handleLook
+import org.realparkourhelper.handlers.handlePosition
+import org.realparkourhelper.handlers.handlePositionLook
 
 class ReplayRecording: JavaPlugin() {
 
@@ -46,7 +54,7 @@ class ReplayRecording: JavaPlugin() {
                     PacketType.Play.Client.LOOK -> handleLook(event, recorder)
                     PacketType.Play.Client.POSITION_LOOK -> handlePositionLook(event, recorder)
                     PacketType.Play.Client.BLOCK_DIG -> handleBlockDig(event, recorder)
-                    PacketType.Play.Client.HELD_ITEM_SLOT -> handleHeldItemChange(event, recorder)
+                    PacketType.Play.Client.HELD_ITEM_SLOT -> handleHeldItemSlot(event, recorder)
                     PacketType.Play.Client.ARM_ANIMATION -> handleArmAnimation(event, recorder)
                     PacketType.Play.Client.ENTITY_ACTION -> handleEntityAction(event, recorder)
                     PacketType.Play.Client.BLOCK_PLACE -> handleBlockPlace(event, recorder)
