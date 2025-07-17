d6fe041683d3b5056c796dcd0ff94eecc69f0fa5
Tauktes <torbenstarn@gmail.com>
2025-07-17 21:46:03 +0200

    add heldItemSlot handler

---
+++ src/main/kotlin/handlers/handleHeldItemSlot.kt
@@ -0,0 +1,21 @@
+package org.realparkourhelper.handlers
+
+import com.comphenix.protocol.events.PacketEvent
+import org.realparkourhelper.PacketTypeEnum
+import org.realparkourhelper.ReplayRecorder
+import org.realparkourhelper.ReplayEvent
+
+fun handleHeldItemSlot(event: PacketEvent, recorder: ReplayRecorder) {
+    val slot = event.packet.shorts.read(0)
+
+    recorder.record(
+        ReplayEvent(
+            timestamp = System.currentTimeMillis(),
+            playerId = event.player.uniqueId,
+            type = PacketTypeEnum.HELD_ITEM_SLOT,
+            data = mapOf(
+                "slot" to slot,
+            )
+        )
+    )
+}
